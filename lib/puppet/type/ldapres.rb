92980d03cf3d1a638fa0015cee1ba9c818aaf2cb
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-10-09 04:45:07 +0100

    Add some other params we're likely to be using.
    i.e.: everything we require to connect to a target LDAP server.

--- lib/puppet/type/ldapres.rb
+++ lib/puppet/type/ldapres.rb
@@ -5,4 +5,20 @@ Puppet::Type.newtype(:ldapres) do
 	newparam(:dn, :namevar => true) do
     desc "The DN of the ldapres we're working on"
   end
+
+  newparam(:binddn) do
+    desc "DN to bind to the LDAP server with"
+  end
+
+  newparam(:bindpw) do
+    desc "Password to bind to the LDAP server with"
+  end
+
+  newparam(:ldapserverhost) do
+    desc "Hostname of ldap server to connect to"
+  end
+
+  newparam(:ldapserverport) do
+    desc "Port number of ldap server we're connecting to"
+  end
 end
