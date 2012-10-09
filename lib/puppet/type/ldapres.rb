9cebb65fed9480c98298bf91192d74269f8d4430
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-10-09 04:41:43 +0100

    Add some basic facilities to ldapres.
    The DN param is going to be the namevar, and as might be obvious, also
    the DN of the thing we're working on in LDAP.

--- lib/puppet/type/ldapres.rb
+++ lib/puppet/type/ldapres.rb
@@ -1,3 +1,8 @@
 Puppet::Type.newtype(:ldapres) do
   @doc = "Insert here: documentation."
+	ensurable
+
+	newparam(:dn, :namevar => true) do
+    desc "The DN of the ldapres we're working on"
+  end
 end
