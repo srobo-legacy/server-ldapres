858a846304197a1fce5a14c0cf2a56c5bdb3d62b
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-10-11 11:42:22 +0100

    Define methods for create/destroying ldap conn.

--- lib/puppet/provider/ldapres/ldapres.rb
+++ lib/puppet/provider/ldapres/ldapres.rb
@@ -2,4 +2,16 @@ Puppet::Type.type(:ldapres).provide :default do
   desc "Default provider for ldapres resources"
 
   confine :true => Puppet.features.ldap?
+
+  def getconnected
+    @conn = LDAP::Conn.new(host=@resource[:ldapserverhost],
+                           port=@resource[:ldapserverport])
+    @conn.bind(dn=@resource[:binddn], password=@resource[:bindpw],
+               method=LDAP::LDAP_AUTH_SIMPLE)
+  end
+
+  def unconnect
+    @conn.unbind
+  end
+
 end
