ac090300fe46ba9bf52801ca8e890b0cd5588a8e
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-10-11 11:37:26 +0100

    Actually, nurk provider class for ldap entirely.
    It would appear that things like '@resource' aren't provided if you
    roll your own, which makes things practically impossible for me to
    understand right now.

--- lib/puppet/provider/ldapres/ldapres.rb
+++ lib/puppet/provider/ldapres/ldapres.rb
@@ -1,11 +1,4 @@
-class Puppet::Provider::Ldapres < Puppet::Provider
-  def initialize(*args)
-    puts "I'm covered in bees"
-    puts args.inspect
-  end
-end
-
-Puppet::Type.type(:ldapres).provide :default, :parent => Puppet::Provider::Ldapres do
+Puppet::Type.type(:ldapres).provide :default do
   desc "Default provider for ldapres resources"
 
   confine :true => Puppet.features.ldap?
